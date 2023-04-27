9ee6ef3ad983f0b7d9bcee3a0d98bd8f4c971fc5
MekalaSreenivasan <mekalasrini.95@gmail.com>
2023-04-27 18:56:05 +0530

    Commit the key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
     production: false,
     firebase: {
-        apiKey: "from firebase config",
-        authDomain: "from firebase config",
-        databaseURL: "from firebase config",
-        projectId: "from firebase config",
-        storageBucket: "from firebase config",
-        messagingSenderId: "from firebase config",
-        appId: "from firebase config",
-        measurementId: "from firebase config"
+        apiKey: "AIzaSyDC_awUkpQIzq_JnifegLEotgVqw71Y2Do",
+        authDomain: "push-notifications-hck23.firebaseapp.com",
+        //databaseURL: "from firebase config",
+        projectId: "push-notifications-hck23",
+        storageBucket: "push-notifications-hck23.appspot.com",
+        messagingSenderId: "723005789203",
+        appId: "1:723005789203:web:04356c56ffb20ff9e61be6",
+        measurementId: "G-39QYWMPNPZ"
     }    
 };
--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,12 +1,12 @@
 importScripts('https://www.gstatic.com/firebasejs/8.0.2/firebase-app.js'); 
 importScripts('https://www.gstatic.com/firebasejs/8.0.2/firebase-messaging.js');
 firebase.initializeApp({ 
-    apiKey: 'from firebase config', 
-    authDomain: "from firebase config", 
-    databaseURL: "from firebase config", 
-    projectId: "from firebase config", 
-    storageBucket: "from firebase config", 
-    messagingSenderId: "from firebase config", 
-    appId: "from firebase config", 
-    measurementId: "from firebase config" 
+    apiKey: 'AIzaSyDC_awUkpQIzq_JnifegLEotgVqw71Y2Do', 
+    authDomain: "push-notifications-hck23.firebaseapp.com", 
+    //databaseURL: "from firebase config", 
+    projectId: "push-notifications-hck23", 
+    storageBucket: "push-notifications-hck23.appspot.com", 
+    messagingSenderId: "723005789203", 
+    appId: "1:723005789203:web:04356c56ffb20ff9e61be6", 
+    measurementId: "G-39QYWMPNPZ" 
 });const messaging = firebase.messaging();

ab9542e0b30880ee914a8218235f653a9a0f5aea
MekalaSreenivasan <mekalasrini.95@gmail.com>
2023-04-27 14:43:18 +0530

    Push Notification app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireMessagingModule } from '@angular/fire/compat/messaging';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireModule } from '@angular/fire/compat';
+import { MessagingService } from '../../service/messaging.service';
+import { environment } from '../environments/environment';
+import { AsyncPipe } from '@angular/common';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireMessagingModule,
+    AngularFireModule.initializeApp(environment.firebase)    
   ],
-  providers: [],
+  providers: [ MessagingService, AsyncPipe ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
